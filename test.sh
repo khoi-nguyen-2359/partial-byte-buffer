bf529235bfaf4fecc7a3c96c869db2b589b624d9
Khoi Nguyen <akhoi90@gmail.com>
2025-12-29 08:16:59 -0500

    Test - Add option to run test with AddressSanitizer

--- test.sh
+++ test.sh
@@ -4,11 +4,22 @@ if [[ ! -d build ]]; then
     mkdir build
 fi
 
-/opt/homebrew/opt/llvm/bin/clang++ -std=c++17 -g -fsanitize=address -fno-omit-frame-pointer \
+CXX="g++"
+SANITIZER_FLAGS=""
+
+if [[ "$ASAN_OPTIONS" == *"detect_leaks=1"* ]]; then
+    SANITIZER_FLAGS="-fsanitize=address -fno-omit-frame-pointer"
+    CXX="clang++"
+fi
+
+echo "Compiling tests with: $CXX, Sanitizer Flags: $SANITIZER_FLAGS"
+
+$CXX -std=c++17 -g \
     pbb/source/*.c \
     pbb/test/*.cpp \
     -Ipbb/include \
     -lfmt -lgtest -lgtest_main -lpthread \
+    $SANITIZER_FLAGS \
     -o build/pbb_tests
 
 if [ $? -ne 0 ]; then
@@ -16,6 +27,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+echo "Executing tests..."
+
 cd build
-export ASAN_OPTIONS=detect_leaks=1
 ./pbb_tests
