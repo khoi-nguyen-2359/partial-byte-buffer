070dd59aa531be3b7e5fc9909eddd04d32ee1e11
Khoi Nguyen <akhoi90@gmail.com>
2026-01-01 11:09:09 -0500

    FLR|Test - Fix range of values in test

--- pbb/test/FloatResizerRangeTest.cpp
+++ pbb/test/FloatResizerRangeTest.cpp
@@ -7,45 +7,49 @@
 class FloatResizerTest : public ::testing::Test {
 };
 
-TEST_F(FloatResizerTest, ResizeFloat_CompressAndRestore_LongitudeRange_RestoreEquivalentValues) {
+TEST_F(FloatResizerTest, ResizeFloat_LongitudeRange_RestoreEquivalentValues) {
     for (int i = 0; i <= 180; ++i) {
-        for (int j = 0; j <= 999999; ++j) {
+        int maxj = (i == 180) ? 0 : 99999;
+        double tolerance = 1e-5;
+        for (int j = 0; j <= maxj; ++j) {
             for (int k = -1; k <= 1; k += 2) {
-                double original = k * (i + j * 1e-6);  // Values from -180.999999 to 180.999999
+                double original = k * (i + j * tolerance);  // Values from -180.00000 to 180.00000
                 int src_exp_bits = 11;
                 int src_mant_bits = 52;
                 int dst_exp_bits = 6;
-                int dst_mant_bits = 27;
+                int dst_mant_bits = 24;
 
                 uint64_t resized = flr_resize_float_double(original, src_exp_bits, src_mant_bits, dst_exp_bits, dst_mant_bits);
                 uint64_t restored = flr_resize_float_long(resized, dst_exp_bits, dst_mant_bits, src_exp_bits, src_mant_bits);
                 qword wq;
                 wq.uint64_val = restored;
 
-                EXPECT_NEAR(original, wq.double_val, 1e-6);
+                EXPECT_NEAR(original, wq.double_val, tolerance);
             }
         }
     }
 }
 
-TEST_F(FloatResizerTest, ResizeFloat_CompressAndRestore_LatitudeRange_RestoreEquivalentValues) {
+TEST_F(FloatResizerTest, ResizeFloat_LatitudeRange_RestoreEquivalentValues) {
     for (int i = 0; i <= 90; ++i) {
-        for (int j = 0; j <= 999999; ++j) {
+        int maxj = (i == 180) ? 0 : 99999;
+        double tolerance = 1e-5;
+        for (int j = 0; j <= maxj; ++j) {
             for (int k = -1; k <= 1; k += 2) {
-                double original = k * (i + j * 1e-6);  // Values from -90.999999 to 90.999999
+                double original = k * (i + j * tolerance);  // Values from -90.00000 to 90.00000
+                // printf("Testing latitude: %f\n", original);
                 int src_exp_bits = 11;
                 int src_mant_bits = 52;
                 int dst_exp_bits = 6;
-                int dst_mant_bits = 26;
+                int dst_mant_bits = 23;
 
                 uint64_t resized = flr_resize_float_double(original, src_exp_bits, src_mant_bits, dst_exp_bits, dst_mant_bits);
                 uint64_t restored = flr_resize_float_long(resized, dst_exp_bits, dst_mant_bits, src_exp_bits, src_mant_bits);
                 qword wq;
                 wq.uint64_val = restored;
 
-                EXPECT_NEAR(original, wq.double_val, 1e-6);
+                EXPECT_NEAR(original, wq.double_val, tolerance);
             }
         }
     }
 }
-
